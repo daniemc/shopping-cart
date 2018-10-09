3c4300c7b9dfa28114d16c9d806829dfe90ee7de
daniemc <daniel.meneses27@gmail.com>
2018-10-09 11:55:17 -0500

    add font awesome package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faShoppingCart,
+  faAlignJustify
+} from "@fortawesome/free-solid-svg-icons";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+library.add(faShoppingCart, faAlignJustify);
+ReactDOM.render(<App />, document.getElementById("root"));
