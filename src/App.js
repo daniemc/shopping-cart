ce0644a99c3a7547da8ac5e13c36dc4e2e0f22dd
Daniel MC <daniel.meneses27@gmail.com>
2018-10-15 22:09:36 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import ShoppingPage from "./components/ShoppingPage";
 import Menu from "./components/Menu";
+import { Route, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   state = {
@@ -17,16 +18,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <NavBar
-          menuIsOpen={this.state.menuIsOpen}
-          onMenuToggle={this.handleMenuToggle}
-        />
-        <div className="content">
-          {this.state.menuIsOpen && <Menu open={this.state.menuIsOpen} />}
-          <ShoppingPage />
+      <BrowserRouter>
+        <div className="container">
+          <NavBar
+            menuIsOpen={this.state.menuIsOpen}
+            onMenuToggle={this.handleMenuToggle}
+            />
+          <div className="content">
+            {this.state.menuIsOpen && <Menu open={this.state.menuIsOpen} />}
+              <Route exact path="/shop" component={ShoppingPage} />
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./menu.css";
+import { Link } from "react-router-dom";
 
 class Menu extends Component {
   state = {};
   render() {
     return (
       <div className="menu">
-        <div className="menu-list-item">Ver carrito</div>
-        <div className="menu-list-item">Ver productos</div>
+        <div className="menu-list-item">
+          <Link to="/shop">Comprar</Link>
+        </div>
+        <div className="menu-list-item">
+          <Link to="/cart">Ver carrito</Link>
+        </div>
+        <div className="menu-list-item">
+          <Link to="/products">Ver productos</Link>
+        </div>
       </div>
     );
   }
