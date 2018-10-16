a12d51a20f0f6ce0ad718645ca8463a6d8a5e7ca
Daniel MC <daniel.meneses27@gmail.com>
2018-10-15 22:29:19 -0500

    add extra components (empty) to test routes, fix some styles in App compoents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ShoppingPage from "./components/ShoppingPage";
+import Cart from "./components/Cart";
+import Products from "./components/Products";
 import Menu from "./components/Menu";
 import { Route, BrowserRouter } from 'react-router-dom';
 
@@ -19,16 +21,25 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="container">
+        <div className="app">
           <NavBar
             menuIsOpen={this.state.menuIsOpen}
             onMenuToggle={this.handleMenuToggle}
             />
-          <div className="content">
-            {this.state.menuIsOpen && <Menu open={this.state.menuIsOpen} />}
-              <Route exact path="/shop" component={ShoppingPage} />
+            <div className="container">
+              { this.state.menuIsOpen &&
+                <div className="sidebar-menu">
+                  <Menu open={this.state.menuIsOpen} />          
+                </div>
+              }
+              <div className="page-content">          
+                <Route exact path="/shop" component={ShoppingPage} />
+                <Route exact path="/cart" component={Cart} />
+                <Route exact path="/products" component={Products} />
+              </div>
+            </div>
+
           </div>
-        </div>
       </BrowserRouter>
     );
   }
---
+++ src/components/Products.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    state = {  }
+    render() { 
+        return ( <div><h1>Products Page</h1></div> );
+    }
+}
+ 
+export default Product;
