7fa8f6d9e41ac025bf03dcf1e6ec607f26a1c14e
Daniel MC <daniel.meneses27@gmail.com>
2018-10-24 22:28:19 -0500

    implement combineReducers

--- src/store.js
+++ src/store.js
@@ -1,32 +1,28 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import reduxThunk from 'redux-thunk';
 
-const reducer = (state, action) => {
+const products = (state = [], action) => {
     if (action.type === "FETCH_PRODUCTS") {
-        return {
-         ...state,
-         products: action.products  
-        };
-    } else if (action.type === "ADD_TO_CART") {
+        return action.products;
+    } 
+    return state;
+};
+
+const cart = (state = [], action) => {
+
+    if (action.type === "ADD_TO_CART") {
         const productInCart = state
-        .cart
         .filter(product => product.id === action.product.id);
         if (productInCart.length === 0) {
-            return {
-                ...state,
-                cart: state.cart.concat(action.product)
-            }
+            return state.concat(action.product);
         } 
 
-        return {
-            ...state,
-                cart: state
-                    .cart
-                    .map(productInCart => {
+        return state
+                .map(productInCart => {
                     productInCart.qty = productInCart.id === action.product.id ? productInCart.qty + 1 : productInCart.qty;
                     return productInCart;
-                })
-        }
+                });
+        
     }
 
     return state;
@@ -35,4 +31,9 @@ const reducer = (state, action) => {
 const logger = store => next => action => next(action);    
 
 
-export default createStore(reducer, { cart: [], products: [] }, applyMiddleware(logger, reduxThunk));
+export default createStore(
+    combineReducers({
+        cart, products
+    }), 
+    applyMiddleware(logger, reduxThunk)
+);
