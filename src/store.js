3e9998374f1b1d5e776776f15bc64b82035e244e
Daniel MC <daniel.meneses27@gmail.com>
2018-10-24 19:23:58 -0500

    add the way to increment qty when add same product more than one time

--- src/store.js
+++ src/store.js
@@ -2,9 +2,24 @@ import { createStore } from 'redux';
 
 const reducer = (state, action) => {
     if (action.type === "ADD_TO_CART") {
+        const productInCart = state
+        .cart
+        .filter(product => product.id === action.product.id);
+        if (productInCart.length === 0) {
+            return {
+                ...state,
+                cart: state.cart.concat(action.product)
+            }
+        } 
+
         return {
             ...state,
-            cart: state.cart.concat(action.product)
+                cart: state
+                    .cart
+                    .map(productInCart => {
+                    productInCart.qty = productInCart.id === action.product.id ? productInCart.qty + 1 : productInCart.qty;
+                    return productInCart;
+                })
         }
     }
 
